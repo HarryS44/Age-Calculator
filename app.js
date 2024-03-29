814902811525996c5dc5778e5ceed3d958d14b78
Harry <harrystone042006@outlook.com>
2024-03-29 09:55:05 +0000

    Years now showing correct number & changed some styles

--- app.js
+++ app.js
@@ -4,30 +4,30 @@ const monthInput = $("#month");
 const yearInput = $("#year");
 const submitBtn = $("#submit-btn");
 
-function checkForm(){
-        if (dayInput.val() && monthInput.val() && yearInput.val()) {
-          submitBtn.prop('disabled', false);
-        } else {
-          submitBtn.prop('disabled', true);
-        }
+function checkForm() {
+  if (dayInput.val() && monthInput.val() && yearInput.val()) {
+    submitBtn.prop("disabled", false);
+  } else {
+    submitBtn.prop("disabled", true);
+  }
 }
 
-dayInput.on('input', checkForm);
-monthInput.on('input', checkForm);
-yearInput.on('input', checkForm);
+dayInput.on("input", checkForm);
+monthInput.on("input", checkForm);
+yearInput.on("input", checkForm);
 
-$('#form-data').submit(function(event) {
-    event.preventDefault(); 
-    
-  
-    var day = dayInput.val();
-    var month = monthInput.val();
-    var year = yearInput.val();
-    
-    console.log('Day:', day);
-    console.log('Month:', month);
-    console.log('Year:', year);
-    
-    // You can now use the retrieved data as needed
-    // For example, you can perform further actions or update the UI
-  });
+$("#form-data").submit(function (event) {
+  const date = new Date();
+  event.preventDefault();
+
+  var day = dayInput.val();
+  var month = monthInput.val();
+  var year = yearInput.val();
+
+  $("#show-days").text(day);
+  $("#show-months").text(month);
+  $("#show-years").text(date.getFullYear() - year);
+
+  // You can now use the retrieved data as needed
+  // For example, you can perform further actions or update the UI
+});
