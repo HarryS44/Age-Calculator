b494b9e42514a184836c45c11ea0a86150a2c94f
Harry <harrystone042006@outlook.com>
2024-04-01 09:05:02 +0100

    JavaScript change & Styling change

--- app.js
+++ app.js
@@ -3,28 +3,14 @@ const dayInput = $("#day");
 const monthInput = $("#month");
 const yearInput = $("#year");
 const submitBtn = $("#submit-btn");
-  const date = new Date();
-function checkForm() {
-  if (dayInput.val() && monthInput.val() && yearInput.val()) {
-    submitBtn.prop("disabled", false);
-  } else {
-    submitBtn.prop("disabled", true);
-  }
-}
+const date = new Date();
 
-dayInput.on("input", checkForm);
-monthInput.on("input", checkForm);
-yearInput.on("input", checkForm);
 
-$("#form-data").submit(function (event) {
 
-  event.preventDefault();
-
-  const day = dayInput.val();
-  const month = monthInput.val();
-  const year = yearInput.val();
-
-  $("#show-days").text(day);
-  $("#show-months").text((date.getMonth() + 1) - month);
-  $("#show-years").text(date.getFullYear() - year);
-});
+  // const day = dayInput.val();
+  // const month = monthInput.val();
+  // const year = yearInput.val();  
+  
+  // $("#show-days").text(day);
+  // $("#show-months").text((date.getMonth() + 1) - month);
+  // $("#show-years").text(date.getFullYear() - year - 1);
