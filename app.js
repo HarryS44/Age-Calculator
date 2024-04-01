d72170f17580dfa22d643ab02f63e6e11442e7eb
Harry <harrystone042006@outlook.com>
2024-04-01 09:49:59 +0100

    JavaScript Redo

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-const formData = $("#form-data");
 const dayInput = $("#day");
 const monthInput = $("#month");
 const yearInput = $("#year");
-const submitBtn = $("#submit-btn");
 const date = new Date();
+day.on("input", testFunction());
+year.on("input", testFunction());
+month.on("input", testFunction());
+
+function testFunction() {  const day = dayInput.val();
+  const month = monthInput.val();
+  const year = yearInput.val();
 
 
 
-  // const day = dayInput.val();
-  // const month = monthInput.val();
-  // const year = yearInput.val();  
-  
-  // $("#show-days").text(day);
-  // $("#show-months").text((date.getMonth() + 1) - month);
-  // $("#show-years").text(date.getFullYear() - year - 1);
+  $("#show-days").text(day);
+}
+
+// $("#show-days").text(day);
+// $("#show-months").text((date.getMonth() + 1) - month);
+// $("#show-years").text(date.getFullYear() - year - 1);
