dad81f51e0d814e3dd0bb789906098f268f1b376
Harry <harrystone042006@outlook.com>
2024-04-03 08:35:31 +0100

    JavaScript changes

--- app.js
+++ app.js
@@ -2,17 +2,37 @@ const dayInput = $("#day");
 const monthInput = $("#month");
 const yearInput = $("#year");
 const date = new Date();
-day.on("input", testFunction());
-year.on("input", testFunction());
-month.on("input", testFunction());
 
-function testFunction() {  const day = dayInput.val();
+$("#day").on("input", testFunction);
+$("#month").on("input", testFunction);
+$("#year").on("input", testFunction);
+
+function error() {
+  $("#error-message").removeClass("hidden");
+  setTimeout(function () {
+    $("#error-message").addClass("hidden");
+  }, 1000);
+}
+
+function testFunction() {
+  const day = dayInput.val();
   const month = monthInput.val();
   const year = yearInput.val();
 
+  if (day <= 31) {
+    $("#show-days").text(day);
+  } else {
+    error();
+  }
 
+  if (month <= 12) {
+    $("#show-years").text(year);
+  } else {
+    error();
+  }
 
-  $("#show-days").text(day);
+  $("#show-months").text(month);
+  $("#show-years").text(year);
 }
 
 // $("#show-days").text(day);
